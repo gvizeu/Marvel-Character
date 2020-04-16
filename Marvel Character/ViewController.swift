8bcb029da7956eb6be716b291267c696406a358a
Gonzalo Vizeu <gonzalo.vizeu@talentomobile.com>
2020-04-16 13:34:56 +0200

    Initial Commit

---
+++ Marvel Character/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel Character
+//
+//  Created by Gonzalo Vizeu on 16/04/2020.
+//  Copyright © 2020 Gonzalo Vizeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
